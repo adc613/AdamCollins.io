1fe7d7a1313401092d7f173e6968f7a88e8b39ac
Adam Collins <adc613@gmail.com>
2016-11-19 23:26:29 -0500

    adds parallex affect to story page

--- node/src/pages/Story.jsx
+++ node/src/pages/Story.jsx
@@ -13,16 +13,63 @@ class StoryPage extends React.Component {
   constructor(props) {
     super(props);
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    this.updatePosition = this.updatePosition.bind(this);
+  }
+
+  componentDidMount() {
+
+    let image = document.getElementById('background-banner-image');
+
+    image.addEventListener('load', () => { this.initializePositions(); });
+
+
+  }
+
+  initializePositions() {
+
+    let image = document.getElementById('background-banner-image');
+    let offset = image.offsetHeight;
+    let container = document.getElementById('floating-container');
+
+    this.marginTop = offset / 2;
+    this.offset = offset / 2;
+
+    container.style.marginTop = this.marginTop + 'px';
+
+    //document.addEventListener('scroll', this.updatePosition);
+    window.onscroll = this.updatePosition;
+
+  }
+
+  updatePosition(event) {
+
+    let container = document.getElementById('floating-container');
+    let image = document.getElementById('background-banner-image');
+
+    let offset = image.offsetHeight;
+    let position = window.scrollY / 2;
+
+
+
+
+    this.marginTop = Math.min(offset - 20, offset / 2 + position);
+    container.style.marginTop  = this.marginTop + 'px';
+
+    console.log(container.style.marginTop);
+
   }
 
   render() {
     return (
       <div>
         <MyNavbar />
-        <div id="story-page-content">
-          <Bio />
-          <EntrepreneurialExperience />
-          <WorkExperience />
+        <img id='background-banner-image' src="/images/team.png" />
+        <div id="floating-container">
+          <div id="story-page-content">
+            <Bio />
+            <EntrepreneurialExperience />
+            <WorkExperience />
+          </div>
         </div>
       </div>
     );
